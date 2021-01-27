2a42f29965fac4b705290b774b41bb4bdd8c9c16
Constantin Listar <listarcosti@gmail.com>
2021-01-27 21:01:03 +0200

    change deploy source

--- front/admin/scripts/groups.js
+++ front/admin/scripts/groups.js
@@ -1,4 +1,4 @@
-let baseUrl = 'http://ec2-18-196-187-178.eu-central-1.compute.amazonaws.com:8082';
+let baseUrl = 'http://ec2-18-156-163-248.eu-central-1.compute.amazonaws.com/';
 
 (function () {
 	createGroupExitButtonListener();
